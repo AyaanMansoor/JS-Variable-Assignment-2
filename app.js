6ac283c835faaee76fa06c69cfd63b2acd7be36d
AyaanMansoor <154600399+AyaanMansoor@users.noreply.github.com>
2024-03-02 21:40:56 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+//Q1
+//let age="I am 16 years old"
+//alert(age)
+
+
+//Q3
+//let birthYear="2007"
+//document.write("My birth year is "+birthYear)
+
+//Q4
+ //var visitorName="John Doe";
+ //var productTitle="T-Shirt";
+ //var quantity="5";
+
+ //document.write(`${visitorName} ordered ${quantity} ${productTitle}(s) on XYZ Clothing store.`)
+
